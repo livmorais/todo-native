533885b3cd4e4736b846af782ab5ef285d7d421f
livmorais <liviasantosmorais@gmail.com>
2024-06-14 13:45:50 -0300

    limpeza de arquivos antigos

--- services/supabase.ts
+++
@@ -1,8 +0,0 @@
-import "react-native-url-polyfill/auto"
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "URL";
-const supabaseKey = "KEY";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
